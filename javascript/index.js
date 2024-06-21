6ac65e067fc0965b25bbf220229de4e5df9af05f
azizurrehman2430 <azizurrehman2430@gmail.com>
2024-06-21 18:45:25 +0500

    update website services

---
+++ javascript/index.js
@@ -0,0 +1,23 @@
+var typed = new Typed(".text",{
+    strings: ["Web Developer" , "Graphic Designer" ,"Programmer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+var typed = new Typed(".text1",{
+    strings: ["Web Developer" , "Graphic Designer" ,"Programmer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+
+
+    userName = document.getElementById("userName").value
+    function senddata() {
+        console.log(userName)
+    }
+
+
